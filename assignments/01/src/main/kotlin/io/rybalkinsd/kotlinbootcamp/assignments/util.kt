3bd7b752cf9ae28d938617e4e7c97585d8522709
kecholga <kecholga@gmail.com>
2018-09-28 15:45:29 +0300

    Assignment 1 done

--- assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
+++ assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
@@ -6,7 +6,8 @@ package io.rybalkinsd.kotlinbootcamp.assignments
  * @param values an argument. !! Assume values.length > 0. !!
  * @return the largest of values.
  */
-fun max(values: List<Int>): Int = TODO()
+fun max(values: List<Int>): Int =
+        values.fold(Int.MIN_VALUE) { maxv, v -> if (v>maxv) v else maxv }
 
 /**
  * Returns the sum of all `int` values.
@@ -14,4 +15,5 @@ fun max(values: List<Int>): Int = TODO()
  * @param values an argument. Assume values.length > 0.
  * @return the sum of all values.
  */
-fun sum(values: List<Int>): Long = TODO()
+fun sum(values: List<Int>): Long =
+        values.fold(0L) { total, v -> total + v }
