bfdf388c2f0e0ef1f999502361116cb2da97a3c8
kecholga <kecholga@gmail.com>
2018-10-07 20:40:05 +0300

    Assignment02 done

--- assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
+++ assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
@@ -1,5 +1,7 @@
 package io.rybalkinsd.kotlinbootcamp.geometry
 
+import kotlin.math.min
+import kotlin.math.max
 /**
  * Entity that can physically intersect, like flame and player
  */
@@ -10,13 +12,13 @@ interface Collider {
 /**
  * 2D point with integer coordinates
  */
-class Point(x: Int, y: Int) : Collider {
-    override fun isColliding(other: Collider): Boolean {
-        TODO("not implemented")
-    }
-    fun isInside(borde: List<Point>): Boolean {
-        
-    }
+data class Point(val x: Int,val y: Int) : Collider {
+    override fun isColliding(other: Collider): Boolean =
+            when (other){
+                is Point -> this == other
+                is Bar -> other.isColliding(this)
+                else -> false
+            }
 }
 
 /**
@@ -26,7 +28,31 @@ class Point(x: Int, y: Int) : Collider {
  * (It does not matter, which opposite corners you choose to define bar)
  */
 class Bar(firstCornerX: Int, firstCornerY: Int, secondCornerX: Int, secondCornerY: Int) : Collider {
-    override fun isColliding(other: Collider): Boolean {
-        TODO("not implemented")
+    val bottomLeft = Point (min (firstCornerX, secondCornerX), min(firstCornerY,secondCornerY))
+    val topRight = Point (max (firstCornerX, secondCornerX), max(firstCornerY,secondCornerY))
+
+    val bottomRight
+        get() = Point (topRight.x, bottomLeft.y)
+    val topLeft
+        get() = Point(bottomLeft.x, topRight.y)
+
+    override fun isColliding(other: Collider): Boolean =
+            when (other){
+                is Point -> other.x in bottomLeft.x..topRight.x && other.y in bottomLeft.y..topRight.y
+                is Bar -> listOf(bottomLeft, bottomRight, topLeft, topRight).any{other.isColliding(it)}||
+                          listOf(other.bottomLeft, other.bottomRight, other.topLeft, other.topRight).any {this.isColliding(it)}
+                else -> false
+            }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Bar
+
+        if (bottomLeft != other.bottomLeft) return false
+        if (topRight != other.topRight) return false
+
+        return true
     }
 }
